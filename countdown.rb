43d5c7bdcdd51548ad96796bdb90309b30b3a455
Devon Darrow <ddarrowphl@gmail.com>
2018-08-13 22:49:38 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -1,9 +1,9 @@
 #write your code here
 
 def countdown(seconds)
-  counter = 0
+  counter = 10
   while counter < 10
     puts "#{number} SECOND(S)!"
-    counter -= 10
+    counter -= 1
   end
 end
